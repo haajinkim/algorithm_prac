270594d6843106972a49d880713d4e581ee17acb
hajin <f-a-k@naver.com>
2022-06-29 08:14:30 +0900

    오타수정

--- 모의고사.py
+++ 모의고사.py
@@ -41,12 +41,12 @@ def solution(answers):
 ## 모범 답안
 
 def solution(answers):
-	pattern1 = [1,2,3,4,5]
+    pattern1 = [1,2,3,4,5]
     pattern2 = [2,1,2,3,2,4,2,5]
     pattern3 = [3,3,1,1,2,2,4,4,5,5]
     score = [0,0,0]
-    resullt = []
-    
+    result = []
+
     for idx, answer in enumerate(answers):
         if answer == pattern1[idx%len(pattern1)]:
             score[0] += 1
@@ -54,7 +54,7 @@ def solution(answers):
             score[1] += 1
         if answer == pattern3[idx%len(pattern3)]:
             score[2] += 1   
-    for idx, s in enumerate(score):
-        if s == max(score):
-            result.append(idx+1)
-    return result
+        for idx, s in enumerate(score):
+            if s == max(score):
+                result.append(idx+1)
+                return result
