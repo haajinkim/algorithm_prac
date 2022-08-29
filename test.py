2b058925c33f609e794516a36c672872a93a8e24
hajin <f-a-k@naver.com>
2022-08-29 15:29:45 +0900

    2022.08.29

--- test.py
+++ test.py
@@ -1,30 +1,20 @@
-array_a = [1, 2, 3, 5]
-array_b = [4, 6, 7, 8]
+import math
 
+def solution(n, k):
+    answer = []
+    numberList = [i for i in range(1, n+1)]
 
-def merge(array1, array2):
-    result = []
-    array1_index = 0
-    array2_index = 0
-    while array1_index < len(array1) and array2_index < len(array2):
-        if array1[array1_index] < array2[array2_index]:
-            result.append(array1[array1_index])
-            array1_index += 1
-        else:
-            result.append(array2[array2_index])
-            array2_index += 1
+    while (n != 0):
+        temp = math.factorial(n) // n 
+        index = k // temp
+        k = k % temp
+        if k == 0:
+            answer.append(numberList.pop(index-1))
+        else :
+            answer.append(numberList.pop(index))
+        print(answer)
+        n -= 1
+    
+    return answer
 
-    if array1_index == len(array1):
-        while array2_index < len(array2):
-            result.append(array2[array2_index])
-            array2_index += 1
-
-    if array2_index == len(array2):
-        while array1_index < len(array1):
-            result.append(array1[array1_index])
-            array1_index += 1
-
-    return result
-
-
-print(merge(array_a, array_b))
+print(solution(3,5))
